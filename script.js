796af6a011fb1257118d6621a314affbc0f0728e
RosaBen <benchabane.rosa@outlook.com>
2026-07-23 17:59:04 +0200

    working on script to create card per article - mobile first

--- script.js
+++ script.js
@@ -44,6 +44,53 @@ const data = [{
 
 const mainMobile = document.querySelector(".products");
 
+// main title
 const h2Soldes = document.createElement("h2");
 h2Soldes.textContent = "Soldes";
 mainMobile.appendChild(h2Soldes);
+
+
+// main article
+
+data.map((item, index) => {
+  const articleElement = document.createElement("article");
+  articleElement.classList.add("card");
+
+  const aTopElement = document.createElement("a");
+  aTopElement.classList.add("top-card");
+  aTopElement.href = `#product${index + 1}`;
+
+  const imgElement = document.createElement("img");
+  imgElement.src = item.image;
+  imgElement.alt = item.title;
+
+  const h3Element = document.createElement("h3");
+  h3Element.textContent = item.title;
+
+  const aBottomElement = document.createElement("a");
+  aBottomElement.classList.add("bottom-card");
+  aBottomElement.href = `#product${index + 1}`;
+
+  const h4Element = document.createElement("h4");
+  h4Element.textContent = item.brand;
+
+  const divElement = document.createElement("div");
+
+  const pElement = document.createElement("p");
+  pElement.textContent = item.price;
+
+
+
+  divElement.appendChild(pElement);
+
+  aTopElement.appendChild(imgElement);
+  aTopElement.appendChild(h3Element);
+
+  aBottomElement.appendChild(h4Element);
+  aBottomElement.appendChild(divElement);
+
+  articleElement.appendChild(aTopElement);
+  articleElement.appendChild(aBottomElement);
+
+  mainMobile.appendChild(articleElement);
+});
