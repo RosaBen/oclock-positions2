cb4e8893165cffe2dc0c2d6866e30d596dad25c6
RosaBen <benchabane.rosa@outlook.com>
2026-07-23 17:18:07 +0200

    added html and data in script

---
+++ script.js
@@ -0,0 +1,43 @@
+const data = [{
+  image: "./images/bottines-zip-cuir-taupe.jpg",
+  title: "Bottines zip velours taupe",
+  brand: "Kennel &amp; Schmenger",
+  price: 240
+},
+{
+  image: "./images/bottines-marcia-bleu.jpg",
+  title: "Bottines Marcia bleu",
+  brand: "Emma go",
+  price: 195
+
+},
+{
+  image: "./images/bottines-marron.jpg",
+  title: "Bottines marron élastique multi",
+  brand: "Anniel",
+  price: 195
+
+},
+{
+  image: "./images/bottines-cuir.jpg",
+  title: "Bottines cuir paillettes",
+  brand: "Rovial",
+  price: 195
+
+},
+{
+  image: "./images/bottines-franges.jpg",
+  title: "Bottines franges et médailles",
+  brand: "Minnetonka",
+  price: 195
+
+},
+{
+  image: "./images/bottines-anna-python.jpg",
+  title: "Bottines Anna Python",
+  brand: "Emma go",
+  price: 185
+
+},
+
+];
